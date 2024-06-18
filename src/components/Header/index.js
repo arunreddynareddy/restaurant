85b488ebd0c540bf1d4677855335130ab6ebe0e9
arunreddynareddy <arunreddynareddy@gmail.com>
2024-06-18 01:32:45 +0000

    enhancement of restaurant app

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,27 +1,54 @@
+import {useContext} from 'react'
+import {Link, withRouter} from 'react-router-dom'
 import {AiOutlineShoppingCart} from 'react-icons/ai'
+import Cookies from 'js-cookie'
+
+import CartContext from '../../context/CartContext'
+
 import './index.css'
 
 const Header = props => {
-  const {cartItems} = props
-  const getCartCount = () => {
-    const count = cartItems.reduce((acc, item) => acc + item.quantity, 0)
-    return count
+  const {cartList, restaurantName} = useContext(CartContext)
+
+  const onLogout = () => {
+    const {history} = props
+    Cookies.remove('jwt_token')
+    history.replace('/login')
   }
 
-  return (
-    <header className="header-container">
-      <h1 className="header-main-heading">UNI Resto Cafe</h1>
-      <div className="description-icon-container">
-        <p className="header-description">My Orders</p>
-        <div className="cart-icon-container">
+  const renderCartIcon = () => (
+    <div className="cart-icon-link">
+      <Link to="/cart">
+        <button type="button" className="cart-icon-button" data-testid="cart">
           <AiOutlineShoppingCart className="cart-icon" />
-          <div className="cart-count-badge">
-            <p className="cart-count">{getCartCount()}</p>
-          </div>
-        </div>
+        </button>
+      </Link>
+      <div className="cart-count-badge d-flex justify-content-center align-items-center">
+        <p className="m-0 cart-count">{cartList.length}</p>
+      </div>
+    </div>
+  )
+
+  return (
+    <header className="p-4 d-flex flex-row align-items-center nav-header">
+      <Link to="/">
+        <h1 className="m-0 logo-heading">{restaurantName}</h1>
+      </Link>
+      <div className="d-flex flex-row align-items-center ms-auto">
+        <p className="mt-0 mb-0 me-2 d-none d-sm-block my-orders-text">
+          My Orders
+        </p>
+        <button
+          type="button"
+          className="btn btn-outline-danger ms-2 me-2 btn-sm"
+          onClick={onLogout}
+        >
+          Logout
+        </button>
+        {renderCartIcon()}
       </div>
     </header>
   )
 }
 
-export default Header
+export default withRouter(Header)
--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,95 +1,65 @@
-import {useState, useEffect} from 'react'
-import Loader from 'react-loader-spinner'
-import './index.css'
+import {useState, useEffect, useContext} from 'react'
 
 import Header from '../Header'
 import DishItem from '../DishItem'
+
+import CartContext from '../../context/CartContext'
+
+import './index.css'
 
 const Home = () => {
   const [isLoading, setIsLoading] = useState(true)
   const [response, setResponse] = useState([])
   const [activeCategoryId, setActiveCategoryId] = useState('')
-  const [cartItems, setCartItems] = useState([])
 
-  const addItemToCart = dish => {
-    const isDishPresent = cartItems.find(
-      eachDish => eachDish.dishId === dish.dishId,
-    )
+  const {cartList, setRestaurantName} = useContext(CartContext)
 
-    if (!isDishPresent) {
-      const newDish = {...dish, quantity: 1}
-      setCartItems(prev => [...prev, newDish])
-    } else {
-      setCartItems(prev =>
-        prev.map(item =>
-          item.dishId === dish.dishId
-            ? {...item, quantity: item.quantity + 1}
-            : item,
-        ),
-      )
-    }
-  }
-
-  const removeItemFromCart = dish => {
-    const isDishExists = cartItems.find(
-      eachItem => eachItem.dishId === dish.dishId,
-    )
-
-    if (isDishExists) {
-      setCartItems(prev =>
-        prev
-          .map(eachDish =>
-            eachDish.dishId === dish.dishId
-              ? {...eachDish, quantity: eachDish.quantity - 1}
-              : eachDish,
-          )
-          .filter(eachDish => eachDish.quantity > 0),
-      )
-    }
-  }
-
-  const getFormattedData = tableMenuList =>
+  const getUpdatedData = tableMenuList =>
     tableMenuList.map(eachMenu => ({
       menuCategory: eachMenu.menu_category,
       menuCategoryId: eachMenu.menu_category_id,
       menuCategoryImage: eachMenu.menu_category_image,
       categoryDishes: eachMenu.category_dishes.map(eachDish => ({
-        dishAvailability: eachDish.dish_Availability,
-        dishType: eachDish.dish_Type,
-        dishCalories: eachDish.dish_calories,
-        dishCurrency: eachDish.dish_currency,
-        dishDescription: eachDish.dish_description,
         dishId: eachDish.dish_id,
-        dishImage: eachDish.dish_image,
         dishName: eachDish.dish_name,
         dishPrice: eachDish.dish_price,
+        dishImage: eachDish.dish_image,
+        dishCurrency: eachDish.dish_currency,
+        dishCalories: eachDish.dish_calories,
+        dishDescription: eachDish.dish_description,
+        dishAvailability: eachDish.dish_Availability,
+        dishType: eachDish.dish_Type,
         addonCat: eachDish.addonCat,
       })),
     }))
 
-  const getFetchedData = async () => {
-    const url = 'https://run.mocky.io/v3/72562bef-1d10-4cf5-bd26-8b0c53460a8e'
-    const apiResponse = await fetch(url)
+  const fetchRestaurantApi = async () => {
+    const api = 'https://run.mocky.io/v3/72562bef-1d10-4cf5-bd26-8b0c53460a8e'
+    const apiResponse = await fetch(api)
     const data = await apiResponse.json()
-    console.log(data)
-    const updatedData = getFormattedData(data[0].table_menu_list)
+    const updatedData = getUpdatedData(data[0].table_menu_list)
     setResponse(updatedData)
+    setRestaurantName(data[0].restaurant_name)
     setActiveCategoryId(updatedData[0].menuCategoryId)
     setIsLoading(false)
   }
 
   useEffect(() => {
-    getFetchedData()
+    fetchRestaurantApi()
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [])
 
-  const onUpdateActiveCategoryId = menuCategoryId =>
+  const onUpdateActiveCategoryIdx = menuCategoryId =>
     setActiveCategoryId(menuCategoryId)
 
-  const renderMenuCategoryView = () =>
+  const addItemToCart = () => {}
+
+  const removeItemFromCart = () => {}
+
+  const renderTabMenuList = () =>
     response.map(eachCategory => {
-      const onClickCategory = () =>
-        onUpdateActiveCategoryId(eachCategory.menuCategoryId)
+      const onClickHandler = () =>
+        onUpdateActiveCategoryIdx(eachCategory.menuCategoryId)
 
       return (
         <li
@@ -99,9 +69,12 @@ const Home = () => {
               : ''
           }`}
           key={eachCategory.menuCategoryId}
-          onClick={onClickCategory}
+          onClick={onClickHandler}
         >
-          <button type="button" className="menu-tab-button">
+          <button
+            type="button"
+            className="mt-0 mb-0 ms-2 me-2 tab-category-button"
+          >
             {eachCategory.menuCategory}
           </button>
         </li>
@@ -114,12 +87,11 @@ const Home = () => {
     )
 
     return (
-      <ul className="dishes-list-container">
+      <ul className="m-0 d-flex flex-column dishes-list-container">
         {categoryDishes.map(eachDish => (
           <DishItem
             key={eachDish.dishId}
             dishDetails={eachDish}
-            cartItems={cartItems}
             addItemToCart={addItemToCart}
             removeItemFromCart={removeItemFromCart}
           />
@@ -128,18 +100,18 @@ const Home = () => {
     )
   }
 
-  const renderLoaderSpinner = () => (
-    <div className="loader-spinner">
-      <Loader type="ThreeDots" color="#000000" height="50" weight="50" />
+  const renderSpinner = () => (
+    <div className="spinner-container">
+      <div className="spinner-border" role="status" />
     </div>
   )
 
   return isLoading ? (
-    renderLoaderSpinner()
+    renderSpinner()
   ) : (
-    <div className="home-container">
-      <Header cartItems={cartItems} />
-      <ul className="menu-list-container">{renderMenuCategoryView()}</ul>
+    <div className="home-background">
+      <Header cartItems={cartList} />
+      <ul className="m-0 ps-0 d-flex tab-container">{renderTabMenuList()}</ul>
       {renderDishes()}
     </div>
   )
